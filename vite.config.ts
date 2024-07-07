7b0ef0833c1a19ca04c758dee72fff203f90fe1d
Marco Girometta <girometta.marco@gmail.com>
2024-07-08 01:59:18 +0200

    build: configure typescript and vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: "src/validation.ts",
+      formats: ['es', 'cjs']
+    },
+    outDir: "dist",
+  }
+})
