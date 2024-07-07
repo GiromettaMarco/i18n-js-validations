e4e3eeabd9cd17f0e8f6281459ec988b67fb82ca
Marco Girometta <girometta.marco@gmail.com>
2024-07-08 01:59:52 +0200

    test: add a simple test

---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import { Validation } from "../src/validation.ts";
+
+test("Validation", () => {
+  const $v = new Validation();
+  expect($v.hasErrors()).toBe(false);
+});
